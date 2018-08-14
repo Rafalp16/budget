d78a3cf126eec92752126de83d9ccdfbcf4a645a
Rafalp16 <rafalp16a@gmail.com>
2018-08-14 14:38:43 +0200

    add init, getInput, addItem logic

--- app.js
+++ app.js
@@ -1,4 +1,46 @@
 const budgetController = (function() {
+    const Income = function(id, desc, value) {
+        this.id = id;
+        this.desc = desc;
+        this.value = value;
+    };
+    
+    const Expense = function(id, desc, value) {
+        this.id = id;
+        this.desc = desc;
+        this.value = value;
+    };
+    
+    const data = {
+        items: {
+            exp: [],
+            inc: []
+        },
+        totals: {
+            exp: 0,
+            inc: 0
+        }
+    };
+    
+    return {
+        addItem: function(type, desc, val) {
+            let newItem, id;
+            
+            if(data.items[type].length > 0)
+                id = data.items[type][data.items[type].length - 1].id + 1;
+            else id = 0;
+            
+            if(type === 'inc')
+                newItem = new Income(id, desc, val);
+            else if(type=== 'exp') 
+                newItem = new Expense(id, desc, val);
+            data.items[type].push(newItem);
+            return newItem;
+        },
+        testing: function() {
+            return data.items;
+        }
+    }
 })();
 
 const UIController = (function() {
@@ -14,14 +56,25 @@ const UIController = (function() {
 })();
 
 const controller = (function(budgetCtrl, UICtrl) {
-    const addItem = function() {
-        const input = UICtrl.getInput();
+    const setupEventListeners = function() {
+        document.getElementById('add-item').addEventListener('click', addItem);
+
+        document.addEventListener('keypress', function(e) {
+            if(e.keyCode === 13 || e.which === 13) addItem();
+        });
     }
     
-    document.getElementById('add-item').addEventListener('click', addItem);
+    const addItem = function() {
+        const input = UICtrl.getInput();
+        budgetCtrl.addItem(input.type, input.desc, input.value);
+    }
     
-    document.addEventListener('keypress', function(e) {
-        if(e.keyCode === 13 || e.which === 13) addItem();
-    });
+    return {
+        init: function() {
+            setupEventListeners();
+        }
+    }
     
 })(budgetController, UIController);
+
+controller.init();
